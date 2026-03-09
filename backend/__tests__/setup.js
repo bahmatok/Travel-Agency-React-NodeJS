3c49743422ec59d569642cd9c5cd0bbc26bee3a8
bahmatok <bahmatok@gmail.com>
2026-03-09 19:25:00 +0300

    Modify tests config

--- backend/__tests__/setup.js
+++ backend/__tests__/setup.js
@@ -21,3 +21,23 @@ afterEach(async () => {
     await collections[key].deleteMany({});
   }
 });
+
+
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (args[0] && typeof args[0] === 'string' && args[0].includes('Попытка установить роль admin')) {
+    return;
+  }
+  if (args[0] && typeof args[0] === 'string' && args[0].includes('Попытка создать пользователя')) {
+    return;
+  }
+  originalWarn(...args);
+};
+
+const originalError = console.error;
+console.error = (...args) => {
+  if (args[0] && typeof args[0] === 'string' && args[0].includes('JWT verification error')) {
+    return;
+  }
+  originalError(...args);
+};
--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.js'],
-  verbose: true, 
+  verbose: true,
   setupFilesAfterEnv: ['./__tests__/setup.js'],
+  collectCoverageFrom: [
+    '**/*.js',                
+    '!**/server.js',          
+    '!**/upload.js',         
+    '!**/coverage/**',       
+    '!**/node_modules/**',
+    '!**/jest.config.js',
+    '!**/seed.js',
+  ],
 };
